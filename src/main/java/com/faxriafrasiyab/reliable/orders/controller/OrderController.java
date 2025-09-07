898e210980d468972429d1f5e41c19b4a76546ea
Faxri Afrasiyab <fakhriafrasiyab@gmail.com>
2025-09-07 20:44:50 +0200

    order event sent in post request but now is failing

--- src/main/java/com/faxriafrasiyab/reliable/orders/controller/OrderController.java
+++ src/main/java/com/faxriafrasiyab/reliable/orders/controller/OrderController.java
@@ -1,8 +1,13 @@
 package com.faxriafrasiyab.reliable.orders.controller;
 
+import com.faxriafrasiyab.reliable.orders.config.AppTopics;
 import com.faxriafrasiyab.reliable.orders.dto.OrderDto;
 import com.faxriafrasiyab.reliable.orders.entity.Order;
+import com.faxriafrasiyab.reliable.orders.events.OrderCreatedEvent;
 import com.faxriafrasiyab.reliable.orders.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,8 +18,15 @@ public class OrderController {
 
     private final OrderRepository orderRepository;
 
-    public OrderController(OrderRepository orderRepository) {
+    //    @Autowired
+    private final KafkaTemplate<String, Object> kafka;
+    private final AppTopics ordersTopic;
+
+    public OrderController(OrderRepository orderRepository, KafkaTemplate<String, Object> kafka,
+                           AppTopics ordersTopic) {
         this.orderRepository = orderRepository;
+        this.kafka = kafka;
+        this.ordersTopic = ordersTopic;
     }
 
     @PostMapping
@@ -23,7 +35,21 @@ public class OrderController {
         order.setCustomerEmail(orderDto.customerEmail());
         order.setAmount(orderDto.amount());
         order.setCurrency(orderDto.currency());
-        return orderRepository.save(order);
+        Order orderSaved = orderRepository.save(order);
+
+        OrderCreatedEvent event = new OrderCreatedEvent(order.getId(), order.getCustomerEmail(),
+                order.getAmount(), order.getCurrency());
+
+        kafka.send(ordersTopic.orders(), String.valueOf(orderSaved.getId()), event)
+                .whenComplete((metadata, exception) -> {
+                    if (exception != null) {
+                        System.err.println("Error sending order to topic " + ordersTopic +
+                                "Error message: " + exception.getMessage());
+                    } else {
+                        System.out.println("Event sent to kafka:" + metadata.getRecordMetadata().topic());
+                    }
+                });
+        return orderSaved;
     }
 
     @GetMapping
