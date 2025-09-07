f444cf77c9c62880716134d812f28eb2a765fc6f
Faxri Afrasiyab <fakhriafrasiyab@gmail.com>
2025-09-07 08:32:04 +0200

    Add gitattributes for consistent line endings

---
+++ src/main/java/com/faxriafrasiyab/reliable/orders/ReliableOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.faxriafrasiyab.reliable.orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReliableOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReliableOrdersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faxriafrasiyab/reliable/orders/ReliableOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faxriafrasiyab.reliable.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReliableOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
